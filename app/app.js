0c3ad60f172077c5df4c166f7a6f69034768d403
Gavin Joyce <gavinjoyce@gmail.com>
2017-01-16 13:27:27 +0000

    fixup!

--- app/app.js
+++ app/app.js
@@ -3,11 +3,19 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+debugger;
+
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
+  LOG_TRANSITIONS: true, // basic logging of successful transitions
+  LOG_TRANSITIONS_INTERNAL: true, // detailed logging of all routing steps
+  init() {
+    debugger;
+    this._super(...arguments);
+  },
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver

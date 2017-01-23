04aec5020420d555e665a3416002a51b7455cf8c
Gavin Joyce <gavinjoyce@gmail.com>
2017-01-23 09:07:46 +0000

    fixup!

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('settings', function() {
+    this.route('general');
+    this.route('appearance');
+  })
 });
 
 export default Router;
